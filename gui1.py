7ca7ef0c587654dee9c97555f54f49242e1201bb
kirtimistri <kirtimistri85@gmail.com>
2024-02-14 23:20:12 +0530

    this is my first gui

---
+++ gui1.py
@@ -0,0 +1,9 @@
+from tkinter import *
+window_root=Tk()
+#width x hight y
+window_root.geometry("664x434")
+window_root.minsize(300,100)
+window_root.maxsize(1200,988)
+kirti = Label(text = 'Hey here this is my first GUI ᕙ( ͡° ͜ʖ ͡°)ᕗ')
+kirti.pack()
+window_root.mainloop()
